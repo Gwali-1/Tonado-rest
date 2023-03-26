556478c7c85c089255ff8912fb50c6d3b24f3812
Gwali <gwalisam37@gmail.com>
2023-03-26 22:23:46 +0000

    function to return db connection object and base class finsihed

--- server.py
+++ server.py
@@ -1,20 +1,30 @@
 import tornado.web
 import asyncio
+import tornado.ioloop
+import sqlite3
+
+
+
+
+def get_db_connection():
+    connection = sqlite3.connect("session.sql")
+    cursor= connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY , Name TEXT, Tokens INTEGER DEFAULT 10), Api_Key TEXT ")
+    cursor.close()
+    return connection
+
 
 
 #subhandlers
-
 class BaseHander(tornado.web.RequestHandler):
 
-    async def make_query(self,statement,*args):
+    def make_query(self,statement,*args):
         '''takes sql statement to excute and placeholder arguments to be inserted
-            execute as await make_query()
+            executed asynchronously using torn
         '''
-        with(await self.db.cursor()) as cur:
-            results = await cur.execute(statement,args)
-            if not results:
-                return None
-            return results
+        cursor = self.db.cursor()
+        results = cursor.execute(statement,args)
+        return None if not results else results
         
     
 
@@ -33,7 +43,7 @@ class BaseHander(tornado.web.RequestHandler):
                     self.redirect("/createAccount")
             except Exception as e:
                 self.write(e)
-                
+
                 
 
 
